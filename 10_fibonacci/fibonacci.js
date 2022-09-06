64988db6fabf8a06a72b82dc3aa1bec732fc5575
petrosAth <develop@athanasoulis.me>
2022-09-06 08:30:08 +0300

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+  if (number < 0) {
+    return "OOPS";
+  }
+  let result = 1;
+  let fibonacciArray = [];
+  for (let i = 0; i < number; i++) {
+    fibonacciArray.push(result);
+    if (i === 0) {
+      result = fibonacciArray[i];
+    } else if (i === 1) {
+      result = fibonacciArray[i];
+    } else {
+      result = fibonacciArray[i - 1] + fibonacciArray[i];
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
