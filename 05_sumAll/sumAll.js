6c5de54233af5d57561364e26f7f8079d95b76ce
petrosAth <develop@athanasoulis.me>
2022-09-06 08:28:45 +0300

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function(fromNum, toNum) {
+  if (fromNum < 0 || toNum < 0) {
+    return "ERROR";
+  }
+  if (!(typeof fromNum === "number") || !(typeof toNum === "number")) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (fromNum < toNum) {
+    for (let i = fromNum; i <= toNum; i++) {
+      sum += i;
+    }
+  } else if (fromNum > toNum) {
+    for (let i = toNum; i <= fromNum; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
