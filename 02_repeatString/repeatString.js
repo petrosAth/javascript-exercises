a3446f5ef5b1f9d417b6ed99ebefd4a7b0b45707
petrosAth <develop@athanasoulis.me>
2022-09-06 08:27:53 +0300

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  let multipliedString = string;
+  if (num === 0) {
+    multipliedString = "";
+  } else if (num < 0) {
+    return "ERROR";
+  } else {
+    for (num = num - 1; num > 0; --num) {
+      multipliedString += string;
+    }
+  }
+  return multipliedString;
 };
 
 // Do not edit below this line
