42894f6fc93592bbc5feff820261a7644ab50885
petrosAth <develop@athanasoulis.me>
2022-09-06 08:28:33 +0300

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+  const array = args[0];
+  const arrayWithSplices = args.slice(1);
+  for (const index of arrayWithSplices) {
+    let elementToSplice = array.findIndex((element) => element === index);
+    if (elementToSplice >= 0) {
+      array.splice(elementToSplice, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
