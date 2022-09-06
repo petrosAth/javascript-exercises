fcd02dcad9ba0c5b9ca6237c52e7f7b0b672d0f9
petrosAth <develop@athanasoulis.me>
2022-09-06 08:29:43 +0300

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function(text) {
+  text = text.toLowerCase();
+  text = text.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, "");
+  let textLength = text.length - 1;
+  let reverseText = "";
 
+  for (textLength; textLength >= 0; textLength--) {
+    reverseText += text[textLength];
+  }
+
+  return text === reverseText ? true : false;
 };
 
 // Do not edit below this line
