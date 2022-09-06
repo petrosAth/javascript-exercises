c8fde686d1f9c2ba01a3cb23646ab85a72eb0f49
petrosAth <develop@athanasoulis.me>
2022-09-06 08:29:15 +0300

    Solve 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,19 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
+  let celsius = ((temperature - 32) * 5) / 9;
+  celsius = Math.round(celsius * 10) / 10;
 
+  return celsius;
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
+  let fahrenheit = (temperature * 9) / 5 + 32;
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
 
+  return fahrenheit;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
