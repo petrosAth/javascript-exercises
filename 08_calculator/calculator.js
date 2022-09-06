a3e98577d6032f3e4bfc711348025c1a74b2fa58
petrosAth <develop@athanasoulis.me>
2022-09-06 08:29:23 +0300

    Solve 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+  for (const number of array) {
+    result += number;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (const number of array) {
+    result *= number;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  let powerOfNumber = 1;
+  for (let i = b; i > 0; i--) {
+    powerOfNumber *= a;
+  }
+  return powerOfNumber;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 0) {
+    return number + 1;
+  } else {
+    return number * factorial(number - 1);
+  }
 };
 
 // Do not edit below this line
@@ -29,5 +45,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
