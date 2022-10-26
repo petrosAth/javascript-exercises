a376935267f3ea7e1aa098c049a187cd35cbd61b
petrosAth <develop@athanasoulis.me>
2022-10-26 19:13:38 +0300

    Solve 12_findTheOldest
    Solve 12_findTheOldest
    
    Solve 12_findTheOldest
    
    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  return people.reduce((lastPerson, newPerson) => {
+    let oldest = getAge(lastPerson);
+    let current = getAge(newPerson);
 
+    return current > oldest ? newPerson : lastPerson;
+  });
 };
+
+function getAge(person) {
+  const currentYear = new Date().getFullYear();
+  if (isNaN(person.yearOfDeath)) {
+    return currentYear - person.yearOfBirth;
+  }
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
